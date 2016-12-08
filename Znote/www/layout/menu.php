5a79c5118e848616d0788f105f3c9a643cbcba46
Oliveryo <oliver.bjorklund@bahnhof.se>
2016-12-08 13:22:18 +0100

    menu semi-fix

--- Znote/www/layout/menu.php
+++ Znote/www/layout/menu.php
@@ -66,8 +66,8 @@
 							<?PHP } else { ?>
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account Management<b class="caret"></b></a>
-									<ul class="dropdown-menu"
-										<li><a href="myaccount.php"></a></li>
+									<ul class="dropdown-menu"							
+										<li><a href="myaccount.php">My Account</a></li>
 										<li><a href="createcharacter.php">Create Character</a></li>
 										<li><a href="changepassword.php">Change Password</a></li>
 										<li><a href="settings.php">Settings</a></li>
