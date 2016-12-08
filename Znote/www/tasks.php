5c99f1c9ffd6c15278b91ac213a20aa8dd2b9ed4
hans <hansstake@hotmail.de>
2016-12-08 13:49:31 +0100

    Update tasks.php
    fix 1

--- Znote/www/tasks.php
+++ Znote/www/tasks.php
@@ -4,30 +4,30 @@
 <?php
 $tasks = [
 	1 => [
-    "img" => "amazon",
+		"img" => "amazon",
 		"name" => "Amazons Task",
 		"kills" => "200",
 		"reward" => [
-      1 => "10.000 XP",
-      2 => "5.000 GP",
-    ],
+			1 => "10.000 XP",
+			2 => "5.000 GP",
+		],
 	],
-  2 => [
-    "img" => "bear",
+	2 => [
+		"img" => "bear",
 		"name" => "Bears Task",
 		"kills" => "100",
 		"reward" => [
-      1 => "2.000 XP",
-    ],
+			1 => "2.000 XP",
+		],
 	],
-  3 => [
-    "img" => "orcshaman",
+	3 => [
+		"img" => "orcshaman",
 		"name" => "Orc Shamans Task",
 		"kills" => "400",
 		"reward" => [
-      1 => "2.000 GP",
-      2 => "Blue Spellwand",
-    ],
+			1 => "2.000 GP",
+			2 => "Blue Spellwand",
+		],
 	],
 ];
 
@@ -38,17 +38,25 @@ $tasks = [
 		<th>Monster</th>
 		<th>Name</th>
 		<th>Kills</th>
-    <th>Reward</th>
+		<th>Reward</th>
 	</tr>
-	<?php
-	foreach ($tasks as $task) {
-		echo '<tr>';
-    echo '<td><img src="layout/images/monsters/' . $task['img'] . '.gif"/></td>';
-    echo '<td>' . $task['name'] . '</td>';
-    echo '<td>' . $task['kills'] . '</td>';
-    echo '<td>' . foreach ($task['reward'] as $reward) { echo $reward . '<br>'; } . '</td>';
-    echo '</tr>';
-	}?>
+<?php
+foreach ($tasks as $task) {
+	echo '<tr>';
+	echo '<td><img src="layout/images/monsters/' . $task['img'] . '.gif"/></td>';
+	echo '<td>' . $task['name'] . '</td>';
+	echo '<td>' . $task['kills'] . '</td>';
+	$reward_str = '';
+	foreach ($task['reward'] as $reward) {
+		if $reward_str == '' {
+			$reward_str = $reward;
+		} else {
+			$reward_str = $reward_str . '<br>' . $reward;
+		}
+	}
+	echo '<td>' . $reward_str . '</td>';
+	echo '</tr>';
+}?>
 </table>
 
 <?php include 'layout/overall/footer.php'; ?>
