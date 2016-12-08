61801de50e4ea8e106d973c75d99f43297160c26
hans <hansstake@hotmail.de>
2016-12-08 12:35:20 +0100

    Update stages.php

--- Znote/www/stages.php
+++ Znote/www/stages.php
@@ -6,23 +6,23 @@ $stages = [
 	1 => [
 		"from" => 1,
 		"to" => 20,
-		"rate" => 30
+		"rate" => 30,
 	],
 	2 => [
 		"from" => 21,
 		"to" => 50,
-		"rate" => 20
+		"rate" => 20,
 	],
 	3 => [
 		"from" => 51,
 		"to" => 70,
-		"rate" => 10
+		"rate" => 10,
 	],
 	4 => [
 		"from" => 71,
 		"to" => 999,
-		"rate" => 5
-	]
+		"rate" => 5,
+	],
 ];
 
 var_dump($stages);
