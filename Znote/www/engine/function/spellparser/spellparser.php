f625d2fcb27cebed92b6d7640d84a23f58d0f169
hans <hansstake@hotmail.de>
2016-12-10 16:25:10 +0100

    Update spellparser.php
    first commit

--- Znote/www/engine/function/spellparser/spellparser.php
+++ Znote/www/engine/function/spellparser/spellparser.php
@@ -1,1 +1,10 @@
-
+<?php
+$spells = simplexml_load_file($config['server_path'].'/data/spells/spells.xml');
+// Create our parsed item list
+$spell_list = fopen('spell_list.txt', 'w');
+foreach ($spells->children() as $spell) {
+	if ($spell['words'] && $spell['name'] != NULL) {
+		fwrite($spell_list, $spell['words'].'@'.$item['name'].'@'.$spell['maglv'].'@'.$spell['mana'].PHP_EOL);
+	}
+}
+fclose($spell_list);
