fdd98e5b723787db29a4ee9bc2a19bc96f9ded23
hans <hansstake@hotmail.de>
2016-12-10 16:28:33 +0100

    Update spelllistparser.php
    fix

--- Znote/www/engine/function/spellparser/spelllistparser.php
+++ Znote/www/engine/function/spellparser/spelllistparser.php
@@ -1,13 +1,10 @@
 <?php 
-/* Returns a PHP array $id => 'name'
-	 $items = getItemList();
-	 echo $items[2160]; // Returns 'Crystal Coin'
-	*/
-function getItemList() {
-	$item_list = explode(PHP_EOL, file_get_contents('item_list.txt'));
+
+function getSpellList() {
+	$spell_list = explode(PHP_EOL, file_get_contents('spell_list.txt'));
 	$ia = array();
-	for ($i = 0; $i < count($item_list) - 1; $i++) {
-		$it = explode('@', $item_list[$i]);
+	for ($i = 0; $i < count($spell_list) - 1; $i++) {
+		$it = explode('@', $spell_list[$i]);
 		$ia[(int)$it[0]] = ucfirst($it[1]);
 	}
 	return $ia;
