a4bca171f51f0578be5344ad266135f0dad6bd64
hans <hansstake@hotmail.de>
2016-12-10 17:22:41 +0100

    Update spellparser.php

--- Znote/www/engine/function/spellparser/spellparser.php
+++ Znote/www/engine/function/spellparser/spellparser.php
@@ -4,7 +4,38 @@ $spells = simplexml_load_file($config['server_path'].'/data/spells/spells.xml');
 $spell_list = fopen('spell_list.txt', 'w');
 foreach ($spells->children() as $spell) {
 	if ($spell['words'] && $spell['name'] != NULL) {
-		fwrite($spell_list, $spell['words'].'@'.$item['name'].'@'.$spell['maglv'].'@'.$spell['mana'].$spell['vocation'].PHP_EOL);
+		$voc = {0,0,0,0};
+		if ($spell['vocation']) {
+			foreach ($spell['vocation'] as $vocname) {
+				switch($vocname) {
+					case "Sorcerer":
+						$voc[1] = 1;
+						break;
+					case "Master Sorcerer":
+						$voc[1] = 1;
+						break;
+					case "Druid":
+						$voc[2] = 1;
+						break;
+					case "Elder Druid":
+						$voc[2] = 1;
+						break;
+					case "Paladin":
+						$voc[3] = 1;
+						break;
+					case "Royal Paladin":
+						$voc[3] = 1;
+						break;
+					case "Knight":
+						$voc[4] = 1;
+						break;
+					case "Elite Knight":
+						$voc[4] = 1;
+						break;
+				}
+			}
+		}
+		fwrite($spell_list, $spell['words'].'@'.$item['name'].'@'.$spell['maglv'].'@'.$spell['mana'].$voc[1].$voc[2].$voc[3].$voc[4].PHP_EOL);
 	}
 }
 fclose($spell_list);
